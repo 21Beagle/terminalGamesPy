8ee68819cdd553e79ea471dc2021bc05301aa4ab
Jack Beagle <jackbeagle@live.co.uk>
2021-10-07 17:10:27 +0100

    small additions to slot game, sometiming things figured out

--- slotMachine.py
+++ slotMachine.py
@@ -1,6 +1,14 @@
 import random
+import os
+import time
+import os
 
-slot_values = ['A', 'K', 'Q', 'J', '10', '9', '8', '6', '5', '4', '3', '2']
+import os
+def clear(): os.system('cls') #on Windows System
+clear()
+
+
+slot_values = ['A', 'K', 'Q', 'J', 'T', '9', '8', '6', '5', '4', '3', '2']
 
 slot_values_length = len(slot_values)
 slot_values_weighted = []
@@ -27,12 +35,41 @@ def get_line(number_of_columns = 5, slot_values = slot_values):
         result.append(slot_values[index])
     return result
 
+def get_new_board(rows = 3, columns = 5):
+    results_board.clear()
+    for i in range(0, rows):
+        results = get_line(columns)
+        results_board.append(results)
+    return results_board
 
-for i in range(0, 3):
-    result = get_line()
-    results_board.append(result)
-    print(results_board)
-    print(result)
-    result.clear()
+def print_machine(results_board = []):
+    machine = ""
+    surround = (2 * len(results_board[0]) - 1) * "=" + "\n"
+    machine += surround
 
-print(results_board)
+    for i in results_board:
+        index = results_board.index(i)
+        for j in results_board[index]:
+            jndex = results_board[index].index(j)
+            machine += results_board[index][jndex]
+            machine += " "
+        machine += "\n"
+    machine += surround
+    print(machine)
+    return machine
+
+def get_board_and_print(time_length):
+    results_board = get_new_board()
+    print_machine(results_board)
+    time.sleep(time_length)
+
+
+i = 0.05
+for j in range(0, 14):
+    get_board_and_print(i)
+    clear()
+    i += j * 0.01
+get_board_and_print(i)
+print("Unlucky!")
+
+time.sleep(10)
