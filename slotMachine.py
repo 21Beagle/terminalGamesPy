f7e466a50be10664a48784dd84c58da4cc840ef7
Jack Beagle <jackbeagle@live.co.uk>
2021-10-08 13:40:49 +0100

    Gave colours and did spin

--- slotMachine.py
+++ slotMachine.py
@@ -2,6 +2,7 @@ import random
 import os
 import time
 import os
+from termcolor import colored
 
 import os
 def clear(): os.system('cls') #on Windows System
@@ -9,15 +10,18 @@ clear()
 
 
 slot_values = ['A', 'K', 'Q', 'J', 'T', '9', '8', '6', '5', '4', '3', '2']
+colours = ['red', 'white', 'blue', 'green']
 
 slot_values_length = len(slot_values)
 slot_values_weighted = []
 
 
+
 for i in range(slot_values_length):
     slot_value = slot_values[i]
-    for j in range(1, i+2):
-        slot_values_weighted.append(slot_value)
+    for colour in colours:
+        for j in range(1, i+2):
+            slot_values_weighted.append(colored(slot_value, colour))
 
 slot_values_weighted_length = len(slot_values_weighted)
 
@@ -35,41 +39,65 @@ def get_line(number_of_columns = 5, slot_values = slot_values):
         result.append(slot_values[index])
     return result
 
-def get_new_board(rows = 3, columns = 5):
+def get_new_board(rows = 17, columns = 5):
     results_board.clear()
     for i in range(0, rows):
         results = get_line(columns)
         results_board.append(results)
     return results_board
 
-def print_machine(results_board = []):
-    machine = ""
-    surround = (2 * len(results_board[0]) - 1) * "=" + "\n"
-    machine += surround
+big_board = get_new_board()
+big_rows = len(big_board)
+big_columns = len(big_board[big_rows-1])
 
-    for i in results_board:
-        index = results_board.index(i)
-        for j in results_board[index]:
-            jndex = results_board[index].index(j)
-            machine += results_board[index][jndex]
-            machine += " "
-        machine += "\n"
-    machine += surround
-    print(machine)
-    return machine
-
-def get_board_and_print(time_length):
-    results_board = get_new_board()
-    print_machine(results_board)
-    time.sleep(time_length)
+def get_row(row_number, big_board = big_board):
+    big_rows = len(big_board)
+    big_columns = len(big_board[big_rows-1])
 
 
-i = 0.05
-for j in range(0, 14):
-    get_board_and_print(i)
+    output_string = ""
+    for j in range(big_columns):
+        if j % 2 == 0:
+            y = row_number
+            x = j
+            output_string += big_board[y][x] + " "
+        if j % 2 != 0:
+            y = big_rows - row_number - 2
+            x = j 
+            output_string += big_board[y][x]  + " "
+    output_string += "\n"
+    for j in range(big_columns):
+        if j % 2 == 0:
+            y = row_number + 1
+            x = j
+            output_string += big_board[y][x] + " "
+        if j % 2 != 0:
+            y = big_rows - row_number - 1
+            x = j
+            output_string += big_board[y][x]  + " "
+    output_string += "\n"
+    for j in range(big_columns):
+        if j % 2 == 0:
+            y = row_number + 2
+            x = j
+            output_string += big_board[y][x] + " "
+        if j % 2 != 0:
+            y = big_rows - row_number 
+            x = j
+            output_string += big_board[y][x]  + " "
+    output_string += "\n"
+    
+    return output_string
+
+
+
+timer = 0.05
+for i in range(3, big_rows-1):
+    board = ""
+    board += get_row(i-1)
+    print(board)
+    time.sleep(timer)
+    timer += 0.1
     clear()
-    i += j * 0.01
-get_board_and_print(i)
-print("Unlucky!")
 
-time.sleep(10)
+
